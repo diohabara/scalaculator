d846b366786c6b3c2cbd27d08a414a873e942699
Kadoi Takemaru <diohabara@gmail.com>
2020-07-21 23:01:36 +0900

    Initialize project

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -1,46 +1,11 @@
-import scala.collection.mutable.Stack
+package scala.Scalculator
 
-class Calculator(input: String) {
-  // mutable fields
-  private var result = 0.0
-  private var value = 0.0
-  private var stack = Stack[Double]()
-
-  def parse(): Double = {
-    for (c <- input) {
-      c match {
-        case '+'              => add()
-        case '-'              => sub()
-        case '*'              => mul()
-        case '/'              => div()
-        case '.'              => dec()
-        case c if (c.isDigit) => cal(c)
-        case _ =>
-          throw new IllegalStateException("This character is not illegal")
-      }
-    }
-    result
-  }
-  private def add(): Unit = {
-    println("{result}")
-  }
-  private def sub(): Unit = {
-    println("{result}")
-  }
-  private def mul(): Unit = {
-    println("{result}")
-  }
-  private def div(): Unit = {
-    println("{result}")
-  }
-  private def cal(c: Char): Unit = {
-    value *= 10.0
-    value += c.toDouble
-  }
-  private def dec(): Unit = {
-    print("{result}")
-  }
-  private def Print(): Unit = {
-    println("{result}")
-  }
+class Calculator(args: Array[String]) {
+  def tokenize() = ???
+  def parse() = ???
+  def add(): Unit = ???
+  def sub(): Unit = ???
+  def mul(): Unit = ???
+  def div(): Unit = ???
+  def cal(): Unit = ???
 }
---
+++ src/test/scala/Scalaculator/CalculatorTest.scala
@@ -0,0 +1,5 @@
+package scala.Scalculator
+
+class CalculatorTest() {
+
+}
