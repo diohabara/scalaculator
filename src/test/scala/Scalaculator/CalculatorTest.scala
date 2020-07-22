6bdaeefe1e61fd5f9d2a23edd22225fb05fd14fe
Kadoi Takemaru <diohabara@gmail.com>
2020-07-22 22:50:50 +0900

    Add outline of Scala

--- src/test/scala/Scalaculator/CalculatorTest.scala
+++ src/test/scala/Scalaculator/CalculatorTest.scala
@@ -1,5 +1,16 @@
 package scala.Scalculator
 
-class CalculatorTest() {
+import org.scalatest._
+import flatspec._
+import matchers._
 
+class CalculatorTest() extends AnyFlatSpec with should.Matchers {
+  It should "add numbers" in {
+  }
+  it should "subtract numbers" in {
+  }
+  it should "multiply numbers" in {
+  }
+  it should "divide numbers" in {
+  }
 }
